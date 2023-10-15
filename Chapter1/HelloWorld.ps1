1861cb655999b9035b32715b14691cc18b5c4cc3
Lucien <lucienengineerlife@gmail.com>
2023-10-15 17:05:37 +0800

    feat: update chapter1

--- Chapter1/HelloWorld.ps1
+++ Chapter1/HelloWorld.ps1
@@ -1,4 +1,4 @@
-# Write-Output 'Hello World'
+#Write-Output 'Hello World'
 
-$name = Read-Host -Prompt '請輸入你的名字'
-Write-Output "恭喜 $name ! 成功撰寫第一個 PowerShell 程式碼。"
+$name = Read-Host -Prompt "Please enter your name"
+Write-Output "Congratulations $name! You have written your first code with PowerShell!"
